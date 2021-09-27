999c07b1d1f22948a60bb3c15ca3348f7cf2df00
Sharmila Podury <sharmila.podury@att.com>
2021-09-27 10:45:27 -0700

    Incorrect reason for hw and power failure reboots
    Use last command to check for ungraceful shutdown
    to report reason for power failure.
    
    Get hardware reboot reason from journal logs.
    
    Refactor crash reason code to get information from
    kernel-crash-dump status.
    
    VRVDR-56140
    VRVDR-56148

--- lib/Vyatta/RebootReason.pm
+++ lib/Vyatta/RebootReason.pm
@@ -23,8 +23,10 @@ my $RR_LOG_FILE   = "$LOG_DIR/reboot_reason.log";
 my $II_LOG_FILE   = "$LOG_DIR/install_image.log";
 my $SAVE_LOG_FILE = "/var/log/vyatta/last_reboot_reason.log";
 
-my @LAST_CMD = ( '/usr/bin/last', '-xFR' );
-my @LOG_CMD  = ( 'journalctl',    '-b-1' );
+my @LAST_CMD = ( 'last', '-Fxn2', 'shutdown', 'reboot' );
+my @LOG_VYATTA_CMD =
+  ( 'journalctl', '-b-1', '-t/opt/vyatta/bin/vyatta-shutdown.pl' );
+my @LOG_LOGIN_CMD = ( 'journalctl', '-b-1', '-tsystemd-logind' );
 
 my $POWER_OFF    = 1;
 my $WARM_REBOOT  = 2;
@@ -34,7 +36,10 @@ my $SYSTEM_CRASH = 5;
 my $OTHER        = 6;
 
 my $UNKNOWN_REASON = "Other: Unknown reason for reboot";
-my $CRASH_UNAVAIL  = "System crash: vmcore and dmesg log not available";
+my $UNGRACEFUL_SHUTDOWN =
+  "Other: Reboot after ungraceful shutdown, force reset/poweroff, or crash";
+my $CRASH_UNAVAIL = "System crash: vmcore and dmesg log not available";
+my $POWERKEY      = "Power off: Power key pressed, system was powered down";
 
 sub logit {
     my ( $file, $msg ) = @_;
@@ -75,103 +80,118 @@ sub save_rr_log_file {
 sub check_image_change {
     my ($running_image) = @_;
 
-    return 0 unless ( -e $II_LOG_FILE );
+    my $rr_type   = $OTHER;
+    my $rr_reason = $UNKNOWN_REASON;
+    return ( $rr_type, $rr_reason ) unless ($running_image);
+    return ( $rr_type, $rr_reason ) unless ( -e $II_LOG_FILE );
     my $old_running_image = read_file($II_LOG_FILE);
 
-    my $check = 0;
     if ( $running_image ne $old_running_image ) {
-        logit( $RR_LOG_FILE,
-"System image change: New image $running_image, old image $old_running_image"
-        );
-        $check = 1;
+        $rr_reason =
+"System image change: New image $running_image, old image $old_running_image";
+        return ( $IMAGE_CHANGE, $rr_reason );
     }
     unlink($II_LOG_FILE);
-    return $check;
+    return ( $rr_type, $rr_reason );
 }
 
-sub process_last_output {
-    my @lines;
-    run3( \@LAST_CMD, undef, \@lines );
-    my $reboot = 0;
-    foreach my $line (@lines) {
-        if ( $line =~ /^reboot/ ) {
-            if ( $reboot == 0 ) {
-                $reboot = 1;
-                next;
+sub check_system_crash {
+    my $client = Vyatta::Configd::Client->new();
+    my $tree   = $client->tree_get_full_hash("system kernel-crash-dump");
+
+    if ( $tree->{'kernel-crash-dump'}->{'enable'} ) {
+        my $status = $tree->{'kernel-crash-dump'}->{'status'};
+        if ( $status->{'rebooted-after-system-crash'} ) {
+            my $rr_type = $SYSTEM_CRASH;
+            foreach my $file ( @{ $status->{'crash-dump-files'} } ) {
+                if ( $file->{'index'} == 0 ) {
+                    my $rr_reason =
+"System crash: vmcore and dmesg log saved in $file->{'path'}";
+                    return ( $rr_type, $rr_reason );
+                }
             }
-            return $SYSTEM_CRASH;
+            return ( $rr_type, $CRASH_UNAVAIL );
         }
-        return $COLD_REBOOT
-          if ( $reboot == 1 and $line =~ /^shutdown/ );
+    }
+    return ( $OTHER, $UNKNOWN_REASON );
+}
+
+sub check_hw_reboot {
+    my @lines;
+    my $err;
+    run3( \@LOG_VYATTA_CMD, undef, \@lines, \$err );
+    foreach my $line (@lines) {
+        chomp($line);
+        if ( $line =~ /vyatta-shutdown.pl/ ) {
+            my $reason = ( split( /:/, $line ) )[-1];
+            if ( $reason =~ /^ All hardware systems reboot/ ) {
+                my $rr_reason = "CLI hardware reboot:$reason";
+                return ( $COLD_REBOOT, $rr_reason );
+            }
+        }
+    }
+    return ( $OTHER, $UNKNOWN_REASON );
+}
+
+sub is_graceful_shutdown {
+    my @lines;
+    my $err;
+    my $reboot = 0;
+    run3( \@LAST_CMD, undef, \@lines, \$err );
+    foreach my $line (@lines) {
+        chomp($line);
+        $reboot = 1 if ( $line =~ /^reboot/ );
+        return 1 if ( $reboot == 1 && $line =~ /^shutdown/ );
     }
     return 0;
 }
 
-sub check_reason {
-    my ( $rr_type, $rr_reason ) = @_;
-
-    if (   !defined($rr_type)
-        || !defined($rr_reason)
-        || $rr_type < 1
-        || $rr_type > 6 )
-    {
-        $rr_type   = $OTHER;
-        $rr_reason = $UNKNOWN_REASON;
+sub check_poweroff {
+    if ( is_graceful_shutdown() ) {
+        my @lines;
+        my $err;
+        run3( \@LOG_LOGIN_CMD, undef, \@lines, \$err );
+        foreach my $line (@lines) {
+            if ( $line =~ /System is powering down.$/ ) {
+                return ( $POWER_OFF, $POWERKEY );
+            }
+        }
     }
-    elsif (defined($rr_type)
-        && $rr_type == $SYSTEM_CRASH
-        && !defined($rr_reason) )
-    {
-        $rr_type   = $SYSTEM_CRASH;
-        $rr_reason = $CRASH_UNAVAIL;
+    return ( $OTHER, $UNKNOWN_REASON );
+}
+
+sub check_rr_log {
+    my $rr_type   = $OTHER;
+    my $rr_reason = $UNKNOWN_REASON;
+    return ( $rr_type, $rr_reason ) unless ( -e $RR_LOG_FILE );
+    $rr_reason = read_file($RR_LOG_FILE);
+    if ( $rr_reason =~ /poweroff:/ ) {
+        $rr_type = $POWER_OFF;
+    } elsif ( $rr_reason =~ /reboot:/ ) {
+        $rr_type = $COLD_REBOOT;
     }
     return ( $rr_type, $rr_reason );
 }
 
-sub get_syscrash_info {
-    my $rr_type   = $SYSTEM_CRASH;
-    my $rr_reason = $CRASH_UNAVAIL;
-    my @lines;
-    run3( \@LOG_CMD, undef, \@lines );
-    foreach my $line (@lines) {
-        if ( $line =~ /kdump-tools: saved vmcore in/ ) {
-            my $path = ( split( / /, $line ) )[-1];
-            chomp($path);
-            $rr_reason = "System crash: vmcore and dmesg log saved in $path";
-            last;
+sub get_reboot_reason {
+    my ( $rr_type, $rr_reason ) = check_image_change( get_running_image() );
+
+    if ( $rr_type eq $OTHER ) {
+        ( $rr_type, $rr_reason ) = check_system_crash();
+        if ( $rr_type eq $OTHER ) {
+            ( $rr_type, $rr_reason ) = check_hw_reboot();
+            if ( $rr_type eq $OTHER ) {
+                ( $rr_type, $rr_reason ) = check_rr_log();
+                if ( $rr_type eq $OTHER ) {
+                    ( $rr_type, $rr_reason ) = check_poweroff();
+                    if ( $rr_type eq $OTHER and !is_graceful_shutdown ) {
+                        $rr_type   = $OTHER;
+                        $rr_reason = $UNGRACEFUL_SHUTDOWN;
+                    }
+                }
+            }
         }
     }
-    return ( check_reason( $rr_type, $rr_reason ) );
-}
-
-sub get_reboot_reason_from_log {
-    my $rr_type;
-    my $rr_reason;
-    if ( check_image_change( get_running_image() ) ) {
-        $rr_type = $IMAGE_CHANGE;
-    }
-    return ( check_reason( $rr_type, $rr_reason ) )
-      unless ( -e $RR_LOG_FILE );
-    $rr_reason = read_file($RR_LOG_FILE);
-    if ( $rr_reason =~ /poweroff:/ ) {
-        $rr_type = $POWER_OFF;
-    }
-    elsif ( $rr_reason =~ /reboot:/ ) {
-        $rr_type = $COLD_REBOOT;
-    }
-    return ( check_reason( $rr_type, $rr_reason ) );
-}
-
-sub get_reboot_reason {
-    my $rr_type;
-    my $rr_reason;
-    if ( process_last_output() == $SYSTEM_CRASH ) {
-        ( $rr_type, $rr_reason ) = get_syscrash_info();
-    }
-    else {
-        ( $rr_type, $rr_reason ) = get_reboot_reason_from_log();
-    }
-    ( $rr_type, $rr_reason ) = check_reason( $rr_type, $rr_reason );
     logit( $RR_LOG_FILE, $rr_reason );
     return ( $rr_type, $rr_reason );
 }
